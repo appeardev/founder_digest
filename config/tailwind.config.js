9bdb233413119a77b6c576f431e5a244abf5d7ee
Jorge Diaz <jajadiaz@gmail.com>
2023-06-14 11:38:51 -0600

    made design changes, added a logo and a hero image. Couldnt make the classes on tailwind.config.js file work, but they are present

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,10 +12,17 @@ module.exports = {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
       colors: { // custom color palette for branding
+        'brand-blue': '#18327a',
+        'brand-blue-dark': '#142962',
+        'brand-orange': '#ee9343',
+        'brand-orange-dark': '#c47937',
+        'primary': '#249040',
+        'primary-hover': {
+        '700': '#217B38',
         'primary': 'rgb(79, 70, 229)',
         'primary-hover': {
-          '700': '#a4411c',
-          '500': '#EA9A72'
+        '700': '#a4411c',
+        '500': '#EA9A72'
         }
       }
     },
