2879418db03996ebdf271434e76315a398435491
Jorge Diaz <jajadiaz@gmail.com>
2023-06-14 14:10:51 -0600

    i fixed the tailwind.config.js with the help of chatgpt and also gave me the tailwind rebuild command, and applied button styling and home page style throughout the site.

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -11,22 +11,20 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
-      colors: { // custom color palette for branding
-        'brand-blue': '#18327a',
-        'brand-blue-dark': '#142962',
-        'brand-orange': '#ee9343',
-        'brand-orange-dark': '#c47937',
-        'primary': '#249040',
-        'primary-hover': {
-        '700': '#217B38',
-        'primary': 'rgb(79, 70, 229)',
-        'primary-hover': {
-        '700': '#a4411c',
-        '500': '#EA9A72'
+      colors: {
+          // Custom color palette for branding
+          'brand-blue': '#18327a',
+          'brand-blue-dark': '#142962',
+          'brand-orange': '#ee9343',
+          'brand-orange-dark': '#c47937',
+          'primary': '#249040',
+          'primary-hover': '#217B38',
+          'primary-hover-primary': 'rgb(79, 70, 229)',
+          'primary-hover-primary-hover-700': '#a4411c',
+          'primary-hover-500': '#EA9A72'
         }
-      }
+      },
     },
-  },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
